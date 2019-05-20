a61abd6670130bb6628e3a023da57053c752128d
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-05-20 20:35:22 +0530

    Added Rspec Tests

--- spec/views/bank_accounts/show.html.haml_spec.rb
+++ spec/views/bank_accounts/show.html.haml_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe BankAccount, type: :model do
+RSpec.describe "bank_accounts/show.html.haml", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
