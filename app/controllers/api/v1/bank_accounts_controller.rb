25cb9c2b0b4d6967a04ad157220d527a0b7de51b
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-05-22 15:55:00 +0530

    Add changes to bank_account_controller

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -2,12 +2,17 @@ module Api
   module V1
     class BankAccountsController < ApplicationController
       def new_transaction 
-        data = {
-          amount: params[:amount]
-          transaction_type: params[:transaction_type]
-          bank_account_id: params[:bank_account_id]
-          recipient_id: params[:recipient_id]
-        }
+          amount = params[:amount]
+          transaction_type = params[:transaction_type]
+          bank_account_id = params[:bank_account_id]
+          recipient_id = params[:recipient_id]
+          data = {
+            amount: amount
+            transaction_type: transaction_type
+            bank_account_id: bank_account_id
+            recipient_id: recipient_id
+          }
+        
         errors = ::BankAccounts::ValidateNewTransaction.new(data).execute!
         bank_account = ::BankAccounts::PerformTransaction.new(data).execute! unless errors.size > 0
         render json: { errors: errors }, status: 402 if errors.size > 0
