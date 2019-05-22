53affb73e5466acbe977fe0f45b7df90819027e7
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-05-22 11:39:17 +0530

    DRY bank_accounts_controller.rb

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -7,23 +7,17 @@ module Api
         bank_account_id = params[:bank_account_id]
         recipient_id = params[:recipient_id]
 
-        errors = ::BankAccounts::ValidateNewTransaction.new(
-                    amount: amount,
-                    transaction_type: transaction_type,
-                    bank_account_id: bank_account_id,
-                    recipient_id: recipient_id
-                  ).execute!
-        
-        if errors.size > 0
-          render json: { errors: errors }, status: 402
-        elsif
-          bank_account = ::BankAccounts::PerformTransaction.new(
-                            amount: amount,
-                            transaction_type: transaction_type,
-                            bank_account_id: bank_account_id,
-                            recipient_id: recipient_id
-                          ).execute!
-          render json: { balance: bank_account.balance }
+        data = {
+          amount: amount,
+          transaction_type: transaction_type,
+          bank_account_id: bank_account_id,
+          recipient_id: recipient_id
+        }
+
+        errors = ::BankAccounts::ValidateNewTransaction.new(data).execute!
+        bank_account = ::BankAccounts::PerformTransaction.new(data).execute! unless errors.size > 0
+        render json: { errors: errors }, status: 402 if errors.size > 0
+        render json: { balance: bank_account.balance }  unless errors.size > 0        
         end
       end
     end
