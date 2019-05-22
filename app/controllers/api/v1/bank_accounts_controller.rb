3c87ce13861a39a6364f2afbbb0cc87b632fe932
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-05-22 16:08:51 +0530

    fixed errors

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -2,11 +2,13 @@ module Api
   module V1
     class BankAccountsController < ApplicationController
       def new_transaction     
-        errors = ::BankAccounts::ValidateNewTransaction.new(amount: params[:amount]
+        errors = ::BankAccounts::ValidateNewTransaction.new(
+          amount: params[:amount],
           transaction_type: params[:transaction_type],
           bank_account_id: params[:bank_account_id],
           recipient_id: params[:recipient_id]).execute!
-        bank_account = ::BankAccounts::PerformTransaction.new(amount: params[:amount]
+        bank_account = ::BankAccounts::PerformTransaction.new(
+          amount: params[:amount],
           transaction_type: params[:transaction_type],
           bank_account_id: params[:bank_account_id],
           recipient_id: params[:recipient_id]).execute! unless errors.size > 0
