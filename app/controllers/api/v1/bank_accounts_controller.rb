d83d0c4eac5ffd0c6fcdbb2b366122c394ff3126
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-05-22 13:19:40 +0530

    shorten bank_accounts_controller

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -1,19 +1,13 @@
 module Api
   module V1
     class BankAccountsController < ApplicationController
-      def new_transaction
-        amount = params[:amount]
-        transaction_type = params[:transaction_type]
-        bank_account_id = params[:bank_account_id]
-        recipient_id = params[:recipient_id]
-
+      def new_transaction 
         data = {
-          amount: amount,
-          transaction_type: transaction_type,
-          bank_account_id: bank_account_id,
-          recipient_id: recipient_id
+          amount: params[:amount]
+          transaction_type: params[:transaction_type]
+          bank_account_id: params[:bank_account_id]
+          recipient_id: params[:recipient_id]
         }
-
         errors = ::BankAccounts::ValidateNewTransaction.new(data).execute!
         bank_account = ::BankAccounts::PerformTransaction.new(data).execute! unless errors.size > 0
         render json: { errors: errors }, status: 402 if errors.size > 0
