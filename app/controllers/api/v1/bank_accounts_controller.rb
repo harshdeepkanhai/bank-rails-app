61f9ecffd04a8a16e671bce79fc160cf82163ec3
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-05-22 16:01:22 +0530

    Fix error in bank_accounts_controller

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -1,20 +1,15 @@
 module Api
   module V1
     class BankAccountsController < ApplicationController
-      def new_transaction 
-          amount = params[:amount]
-          transaction_type = params[:transaction_type]
-          bank_account_id = params[:bank_account_id]
-          recipient_id = params[:recipient_id]
-          data = {
-            amount: amount
-            transaction_type: transaction_type
-            bank_account_id: bank_account_id
-            recipient_id: recipient_id
-          }
-        
-        errors = ::BankAccounts::ValidateNewTransaction.new(data).execute!
-        bank_account = ::BankAccounts::PerformTransaction.new(data).execute! unless errors.size > 0
+      def new_transaction     
+        errors = ::BankAccounts::ValidateNewTransaction.new(amount: params[:amount]
+          transaction_type: params[:transaction_type],
+          bank_account_id: params[:bank_account_id],
+          recipient_id: params[:recipient_id]).execute!
+        bank_account = ::BankAccounts::PerformTransaction.new(amount: params[:amount]
+          transaction_type: params[:transaction_type],
+          bank_account_id: params[:bank_account_id],
+          recipient_id: params[:recipient_id]).execute! unless errors.size > 0
         render json: { errors: errors }, status: 402 if errors.size > 0
         render json: { balance: bank_account.balance }  unless errors.size > 0        
       end
